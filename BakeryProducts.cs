59471d8a7eda46d990b96fc35ddda4dd625600a8
LarisaBalaj <65074652+LarisaBalaj@users.noreply.github.com>
2022-06-05 00:56:26 +0300

    Add files via upload

---
+++ BakeryProducts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management_Project
+{
+    public class BakeryProducts : Store
+    {
+        double quantity = 10;
+        double prod_price = 3;
+        string type = "bread";
+        public BakeryProducts()
+        { }
+
+        public int bakerystock(int total, int ordered)
+        {
+            return total - ordered;
+
+        }
+        public override double TotalPrice()
+        {
+            return quantity * prod_price;
+        }
+
+
+    }
+}
---
+++ BakeryProductsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Management_Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management_ProjectTests
+{
+    [TestClass]
+    public class BakeryProductsTests
+    {
+        private int actual;
+        [TestMethod()]
+        public void BakeryProductsTest()
+        {
+            var expected = 4;
+            var stock = new BakeryProducts();
+
+            var actual = stock.bakerystock(10, 6);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
